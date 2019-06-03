a49020cbd1b346f45e7c449a31e019bd4412517d
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-06-03 20:12:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_names = []
+  new_pigeon_data = {}
   data.each do |data_type, spec|
     spec.each do |da_spec, names|
       names.each do |name|
@@ -11,5 +12,7 @@ def nyc_pigeon_organizer(data)
     end 
   end
   pigeon_names.each do |name|
-    
+    new_pigeon_data[name] = {color: [], gender: [], lives: []}
+  end 
+  binding.pry
 end
