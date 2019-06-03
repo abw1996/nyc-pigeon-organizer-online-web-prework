1f29c3eedd721d0b4803f7f50113ef2322feab7b
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-06-03 20:19:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,8 +15,7 @@ def nyc_pigeon_organizer(data)
     new_pigeon_data[namae] = {color: [], gender: [], lives: []}
     data.each do |data_type, spec|
       spec.each do |da_spec, names|
-        if names.include?(namae) 
-          binding.pry
+        if names.include?(namae)
           new_pigeon_data[namae][data_type].push("#{da_spec}")
           binding.pry
         end 
